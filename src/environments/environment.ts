3b994e41e953d4206ca66e504efd7eb76862acb4
dmitri <dmitri>
2018-03-27 17:10:51 +0300

    First

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,11 @@
-export { environment } from './environment.dev'
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAu5HXX6PuW3zNuea_e5IUHjyHTjDPmH9I",
+        authDomain: "epool-26adc.firebaseapp.com",
+        databaseURL: "https://epool-26adc.firebaseio.com",
+        projectId: "epool-26adc",
+        storageBucket: "epool-26adc.appspot.com",
+        messagingSenderId: "1010484424206"
+    }
+  };
